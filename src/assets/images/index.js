5c9bb0e9a309027e08cbee9f7a2d410d6f771cc4
Satyaprakash Das <99628815+sat17ya@users.noreply.github.com>
2025-11-30 12:33:56 +0530

    New pdf and imags added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,16 +1,33 @@
-import Banner1 from "./banner1.jpeg"
-import Banner2 from "./banner2.jpeg"
-import Banner3 from "./banner3.jpeg"
-import Banner4 from "./banner4.jpeg"
-import Banner5 from "./banner5.jpeg"
-import Logo from "./logo.jpg"
-import FooterBg from './bg1.jpg'
-import Director from "./director.jpg"
-import Principal from "./principal.png"
-import Facility1 from "./facility1.jpg"
-import Facility2 from "./facility2.jpg"
-import Facility3 from "./facility3.jpg"
-import FooterLogo from "./footerLogo.png"
-import CalenderImg from "./calender-for-2025-26.webp"
+import Banner1 from "./banner1.jpeg";
+import Banner2 from "./banner2.jpeg";
+import Banner3 from "./banner3.jpeg";
+import Banner4 from "./banner4.jpeg";
+import Banner5 from "./banner5.jpeg";
+import Logo from "./logo.jpg";
+import FooterBg from "./bg1.jpg";
+import Director from "./director.jpg";
+import Principal from "./principal.png";
+import Facility1 from "./facility1.jpg";
+import Facility2 from "./facility2.jpg";
+import Facility3 from "./facility3.jpg";
+import FooterLogo from "./footerLogo.png";
+import CalenderImg from "./calender-for-2025-26.webp";
+import Bal22_23 from "./Balance Sheet 2022-23.jpg";
 
-export{Banner1,Banner2,Banner3,Banner4,Banner5,Logo,FooterBg,Director,Principal,Facility1,Facility2,Facility3,FooterLogo,CalenderImg}
+export {
+  Banner1,
+  Banner2,
+  Banner3,
+  Banner4,
+  Banner5,
+  Logo,
+  FooterBg,
+  Director,
+  Principal,
+  Facility1,
+  Facility2,
+  Facility3,
+  FooterLogo,
+  CalenderImg,
+  Bal22_23,
+};
--- src/assets/pdf/index.js
+++ src/assets/pdf/index.js
@@ -1,4 +1,32 @@
-import CorOrder from "./COR ORDER.pdf"
-import Trust from "./Tirupati trust.pdf"
+import CorOrder from "./COR ORDER.pdf";
+import Trust from "./Tirupati trust.pdf";
+import Bal21_22 from "./BALANCE SHEET 2021-22.pdf";
+import Bal23_24 from "./Balance Sheet 2023-24-9 (1).pdf";
+import STRUCTURAL_STABILITY_CERTIFICATE from "./BUILDING STABILITY CERTIFICATE.pdf";
+import BluePrint from "./HI-TECH PLAN BLUE PRINT.pdf";
+import LandCert from "./Land Certificate.pdf";
+import Landoc from "./land documents.pdf";
+import NonProp from "./Non_Propitory.pdf";
+import WaterDoc from "./SAFE DRINKING WATER CERTIFICATE.pdf";
+import SportDoc from "./sportsEquipmentDocument.pdf";
+import TeacherDoc from "./TEACHER DETAILS UDISE  - Copy PDF.pdf";
+import Res1 from "./Result 2022-23.pdf";
+import Res2 from "./Result 2023-24.pdf";
+import Res3 from "./Result 2024-25.pdf";
+import PTA from "./PTA.pdf";
 
-export {CorOrder,Trust}
+export {
+  CorOrder,
+  Trust,
+  Bal21_22,
+  Bal23_24,
+  STRUCTURAL_STABILITY_CERTIFICATE,
+  BluePrint,
+  LandCert,
+  Landoc,
+  NonProp,
+  WaterDoc,
+  SportDoc,
+  TeacherDoc,
+  Res1,Res2,Res3,PTA
+};
